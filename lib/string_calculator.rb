9c390f07cb87b6884dc00af3d16df404a93dbabc
Priyanka Yadav <priyanka216668@gmail.com>
2024-05-18 10:23:39 +0530

    Adding method StringCalulator#add with basic functionality

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,2 +1,14 @@
 class StringCalculator
+
+    def self.add(numbers)
+        numbers =  parse_numbers(numbers)
+        numbers.sum
+    end
+
+    private
+
+    def self.parse_numbers(numbers)
+        numbers = numbers.split(",")
+        numbers.map(&:to_i)
+    end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,4 +2,14 @@ require 'rspec'
 require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator do
+
+    describe '.add' do
+
+        it "returns sum of numbers" do
+            result = StringCalculator.add("1, 2")
+            expect(result).to eq(3)
+        end
+    end
+
+
 end
